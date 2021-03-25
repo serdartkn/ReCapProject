133e43ece22c4a7af2bbda552e765e8a54b6feca
serdar tekin <kara_bulu@hotmail.com>
2021-03-26 01:16:51 +0300

    12. Gün, Ödev 2

---
+++ Core/CrossCuttingConcerns/Validation/FluentValidation/ValidationTool.cs
@@ -0,0 +1,20 @@
+﻿using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Core.CrossCuttingConcerns.Validation.FluentValidation
+{
+    class ValidationTool
+    {
+        public static void Validate(IValidator validator, object entity)
+        {
+            var context = new ValidationContext<object>(entity); // Burada diyoruz ki nesnesiyi doğrulayacağım.
+            var result = validator.Validate(context); // Burada da diyoruz ki kurallarım contexte'de bulunan nesneyi doğrulayacak.
+            if (!result.IsValid) //Eğer cevap true değilse yani kuralların hepsi uygulanmıyorsa false döner hata fırlar true ise ekleme olur.
+            {
+                throw new ValidationException(result.Errors);
+            }
+        }
+    }
+}
